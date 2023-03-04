07bb43e592718eb5457bec23145a35928f057514
Gabriel Lima <gabriel.rslima10@gmail.com>
2023-03-04 14:35:21 -0300

    feat: adiciona estilos para a página inicial

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,18 +1,22 @@
 import { Subscribe } from "@/app/components/Subscribe";
 
+import styles from "./page.module.css";
 import "./globalStyles.css";
 
 export default function Home() {
 	return (
-		<main>
-			<h1>
-				Se junte à 10.000+ usuários e fique por dentro das notícias de
-				tecnologia e empreendedorismo
-			</h1>
-			<h2>
-				Quer empreender no ramo da tecnologia? Se inscreva na nossa newsletter e
-				receba diariamente novas notícias sobre tecnologia e empreendedorismo
-			</h2>
+		<main className={styles.page}>
+			<div>
+				<h1 className={styles.page__title}>
+					Se junte à 10.000+ usuários e fique por dentro das notícias de
+					tecnologia e empreendedorismo
+				</h1>
+				<h2 className={styles.page__subtitle}>
+					Quer empreender no ramo da tecnologia? Se inscreva na nossa newsletter
+					e receba diariamente novas notícias sobre tecnologia e
+					empreendedorismo
+				</h2>
+			</div>
 			<Subscribe />
 		</main>
 	);
