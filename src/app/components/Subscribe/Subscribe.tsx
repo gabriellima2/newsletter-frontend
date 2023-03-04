2d3cdbedada7d81c2ccbbee99fc4bc0c2d41075e
Gabriel Lima <gabriel.rslima10@gmail.com>
2023-03-04 15:38:57 -0300

    feat: adiciona estilos ao componente Subscribe

--- src/app/components/Subscribe/Subscribe.tsx
+++ src/app/components/Subscribe/Subscribe.tsx
@@ -5,6 +5,8 @@ import { BaseCheckbox } from "../BaseCheckbox";
 import { BaseButton } from "../BaseButton";
 import { BaseField } from "../BaseField";
 
+import styles from "./styles.module.css";
+
 export const Subscribe = () => {
 	const {
 		email,
@@ -17,16 +19,16 @@ export const Subscribe = () => {
 	});
 
 	return (
-		<main>
-			<form onSubmit={handleSubmit}>
-				<BaseField
-					type="text"
-					placeholder="Digite seu melhor email"
-					value={email}
-					onChange={handleEmailChange}
-					labelText="Email *"
-					errorMessage={null}
-				/>
+		<form onSubmit={handleSubmit} className={styles.subscribe}>
+			<BaseField
+				type="text"
+				placeholder="Digite seu melhor email"
+				value={email}
+				onChange={handleEmailChange}
+				labelText="Email *"
+				errorMessage={null}
+			/>
+			<footer className={styles.subscribe__footer}>
 				<BaseCheckbox
 					labelText="Eu aceito receber emails diariamente"
 					checked={userHasAcceptedSendingEmails}
@@ -35,7 +37,7 @@ export const Subscribe = () => {
 				<BaseButton type="submit" disabled={!userHasAcceptedSendingEmails}>
 					Inscrever-se
 				</BaseButton>
-			</form>
-		</main>
+			</footer>
+		</form>
 	);
 };
