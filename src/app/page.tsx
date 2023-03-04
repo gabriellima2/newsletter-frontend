be644a6763635b548280e247062e0a30618cdc95
Gabriel Lima <gabriel.rslima10@gmail.com>
2023-03-04 15:39:16 -0300

    fix: arruma posicionamento dos itens na página

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,19 +5,21 @@ import "./globalStyles.css";
 
 export default function Home() {
 	return (
-		<main className={styles.page}>
-			<div>
-				<h1 className={styles.page__title}>
-					Se junte à 10.000+ usuários e fique por dentro das notícias de
-					tecnologia e empreendedorismo
-				</h1>
-				<h2 className={styles.page__subtitle}>
-					Quer empreender no ramo da tecnologia? Se inscreva na nossa newsletter
-					e receba diariamente novas notícias sobre tecnologia e
-					empreendedorismo
-				</h2>
+		<main className={styles.container}>
+			<div className={styles.page}>
+				<div>
+					<h1 className={styles.page__title}>
+						Se junte à 10.000+ usuários e fique por dentro das notícias de
+						tecnologia e empreendedorismo
+					</h1>
+					<h2 className={styles.page__subtitle}>
+						Quer empreender no ramo da tecnologia? Se inscreva na nossa
+						newsletter e receba diariamente novas notícias sobre tecnologia e
+						empreendedorismo
+					</h2>
+				</div>
+				<Subscribe />
 			</div>
-			<Subscribe />
 		</main>
 	);
 }
